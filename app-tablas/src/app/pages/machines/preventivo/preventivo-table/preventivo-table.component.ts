32a45e6a7fc8da734cb8c0682e3499d96b7bb8d1
Javier Barrera <29jbarrera@gmail.com>
2024-05-09 11:26:41 +0200

    Tabla Mantenimiento Preventivo a falta de css

--- app-tablas/src/app/pages/machines/preventivo/preventivo-table/preventivo-table.component.ts
+++ app-tablas/src/app/pages/machines/preventivo/preventivo-table/preventivo-table.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+import { PreventivoService } from '../../../../service/preventivo.service';
+import { Preventivo } from '../../../../Interfaces/preventivo.interface';
+import { TableModule } from 'primeng/table';
+
 
 @Component({
   selector: 'app-preventivo-table',
   standalone: true,
-  imports: [],
+  imports: [TableModule, CommonModule],
   templateUrl: './preventivo-table.component.html',
   styleUrl: './preventivo-table.component.css'
 })
-export class PreventivoTableComponent {
+export class PreventivoTableComponent implements OnInit {
+  preventivo: Preventivo[] | undefined;
+  searchForm: FormGroup;
 
+  constructor(
+    private PreventivoService: PreventivoService,
+    private fb: FormBuilder
+  ){
+    this.searchForm = this.fb.group({
+      centroMaquina:[''],
+      subfamilia:[''],
+      maquinaria:[''],
+      nivel:[''],
+      posicion:[''],
+      horromMant:[''],
+      horromFinal:[''],
+      kmMant:[''],
+      kmFinal:[''],
+      fecha:[''],
+    });
+  }
+  ngOnInit(){
+      this.actualizarTabla();
+  }
+  async actualizarTabla() {
+    this.preventivo = await this.PreventivoService.getPreventivo();
+  }
 }
