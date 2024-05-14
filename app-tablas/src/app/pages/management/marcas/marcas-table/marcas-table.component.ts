585720cabf9db7f46acea7e4b3616d97a685d5f8
Javier Barrera <29jbarrera@gmail.com>
2024-05-14 11:08:02 +0200

    Table Marcas

---
+++ app-tablas/src/app/pages/management/marcas/marcas-table/marcas-table.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+import { Marcas } from '../../../../Interfaces/marcas.interface';
+import { MarcasService } from '../../../../service/marcas.service';
+
+import { TableModule } from 'primeng/table';
+import { TagModule } from 'primeng/tag';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { HttpClientModule } from '@angular/common/http';
+import { InputTextModule } from 'primeng/inputtext';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { DropdownModule } from 'primeng/dropdown';
+import { FormlyModule } from '@ngx-formly/core';
+import { FormlyPrimeNGModule } from '@ngx-formly/primeng';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-marcas-table',
+  standalone: true,
+  imports: [
+    TableModule,
+    TagModule,
+    IconFieldModule,
+    InputTextModule,
+    InputIconModule,
+    MultiSelectModule,
+    DropdownModule,
+    HttpClientModule,
+    CommonModule,
+    ReactiveFormsModule,
+    ButtonModule,
+    FormsModule,
+    FormlyModule,
+    FormlyPrimeNGModule,
+  ],
+  templateUrl: './marcas-table.component.html',
+  styleUrl: './marcas-table.component.css',
+})
+export class MarcasTableComponent implements OnInit {
+  marcas: Marcas[] | undefined;
+  searchForm: FormGroup;
+
+  constructor(
+    private MarcasService: MarcasService,
+    private fb: FormBuilder
+  ) {
+    this.searchForm = this.fb.group({
+      name: [''],
+    });
+  }
+  ngOnInit() {
+    this.actualizarTabla();
+  }
+
+  async actualizarTabla() {
+    this.marcas = await this.MarcasService.getMarcas();
+  }
+}
+
--- app-tablas/src/app/pages/management/marcas/marcas.component.ts
+++ app-tablas/src/app/pages/management/marcas/marcas.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MarcasTableComponent } from './marcas-table/marcas-table.component';
 
 @Component({
   selector: 'app-marcas',
   standalone: true,
-  imports: [],
+  imports: [MarcasTableComponent],
   templateUrl: './marcas.component.html',
   styleUrl: './marcas.component.css'
 })
