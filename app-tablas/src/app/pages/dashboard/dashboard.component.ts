b73fe79b1b22f3cead5965332be8a6396893f6a0
Javier Barrera <29jbarrera@gmail.com>
2024-05-16 14:05:50 +0200

    Estilo Home / Dashboard

--- app-tablas/src/app/pages/dashboard/dashboard.component.ts
+++ app-tablas/src/app/pages/dashboard/dashboard.component.ts
@@ -7,13 +7,37 @@ import { CardListComponent } from '../../components/card-list/card-list.componen
   standalone: true,
   imports: [CardInformationComponent, CardListComponent],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
 })
 export class DashboardComponent {
-
   dates = [
-    { text: 'Texto 1', date: '2024-05-16', icon: '../../../assets/images/tickVerde.png' },
+    {
+      text: 'Texto 1',
+      date: '2024-05-16',
+      icon: '../../../assets/images/tickVerde.png',
+    },
     { text: 'Texto 2', date: null, icon: null },
-    { text: 'Texto 3', date: '2024-05-18', icon: '../../../assets/images/cruzRoja.png' }
+    {
+      text: 'Texto 3',
+      date: '2024-05-18',
+      icon: '../../../assets/images/cruzRoja.png',
+    },
+  ];
+  dataUltimaEjecucion = [
+    {
+      text: 'Movisat Tenerife',
+      date: '15/05/2024 20:44:12',
+      icon: '../../../assets/images/tickVerde.png',
+    },
+    {
+      text: 'Movisat Villanueva del Pardillo',
+      date: '2/11/2024 18:00:16',
+      icon: '../../../assets/images/cruzRoja.png',
+    },
+    {
+      text: 'Texto 3',
+      date: '2024-05-18',
+      icon: '../../../assets/images/cruzRoja.png',
+    },
   ];
 }
