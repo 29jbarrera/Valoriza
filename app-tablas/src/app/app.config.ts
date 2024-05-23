72d7ef32dbf8330c991d3bbb6045d2a137aca9f4
DamianRV <damian@kissandcode.com>
2024-05-23 10:55:08 +0200

    changes

--- app-tablas/src/app/app.config.ts
+++ app-tablas/src/app/app.config.ts
@@ -3,17 +3,23 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { FormlyModule } from '@ngx-formly/core';
+import { HttpClientModule } from '@angular/common/http';
+// import { ApiModule, Configuration } from './typescript-angular-client-generated';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     importProvidersFrom([
       FormlyModule.forRoot({
-          validationMessages: [{ name: 'required', message: 'This field is required' }],
-          types: [
-            // { name: 'object', component: ObjectTypeComponent },
-          ],
-        }),
-  ]),
+        validationMessages: [
+          { name: 'required', message: 'This field is required' },
+        ],
+        types: [
+          // { name: 'object', component: ObjectTypeComponent },
+        ],
+      }),
+      HttpClientModule,
+      // ApiModule.forRoot(() => new Configuration({basePath: 'http://localhost:5000'}))
+    ]),
     provideRouter(routes),
   ],
 };
